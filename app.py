5569390ec698fa9ffcb4ecd07dfc0e9ac08e8630
Philipp Bosch <hello@pb.io>
2016-03-17 09:54:51 +0100

    Return proper error status code when file cannot be retrieved.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import json
-import os
 from subprocess import Popen, PIPE
 
 from flask import Flask, jsonify, request
@@ -7,6 +6,7 @@ from flask_cors import cross_origin
 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 @cross_origin(headers=['Content-Type'])
@@ -17,10 +17,10 @@ def hello():
 
     output = Popen(['ffprobe', '-print_format', 'json', '-show_entries', 'format=duration', '-i', url.replace('&', '\&')], stdout=PIPE).stdout.read()
     data = json.loads(output)
-    if data.has_key('format'):
+    if 'format' in data:
         return jsonify({'seconds': data['format']['duration']})
     else:
-        return jsonify({'error': 'Unable to retrieve file from URL'})
+        return jsonify({'error': 'Unable to retrieve file from URL'}), 404
 
 
 if __name__ == "__main__":
